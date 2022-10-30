43a43b2ce5712a745016e2fe067f8fb884f28ade
DeeAnnEye <danielashulamithjustin@gmail.com>
2022-10-30 15:52:57 +0530

    add CashFrame

--- src/swingPackage/MainFrame.java
+++ src/swingPackage/MainFrame.java
@@ -413,7 +413,7 @@ public class MainFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        new InputFrame().setVisible(true);
+        new CashFrame().setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
